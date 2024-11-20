b7900dbf276a75f719f46d28ec471620c9d585a0
Nishan <nishan.temp.10@gmail.com>
2024-11-20 12:52:47 +0530

    changes made successfully

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -11,10 +11,10 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.get("/check", protectRoute, checkAuth);
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
-router.get("/check", protectRoute, checkAuth);
 
 export default router;
